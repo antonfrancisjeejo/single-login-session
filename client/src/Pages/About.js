635419dec1b32bfc23b78b33dd86baa542b76fd7
unknown <anton13jeejo@gmail.com>
2021-03-13 22:17:50 +0530

    Initial commit

---
+++ client/src/Pages/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../components/Header";
+
+const About = () => {
+  return (
+    <div className="home">
+      <Header />
+      <h1>About</h1>
+    </div>
+  );
+};
+
+export default About;
