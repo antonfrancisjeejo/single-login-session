d90f34e2bb821f4886ebd8c670c37636bf080742
unknown <anton13jeejo@gmail.com>
2021-03-13 22:17:58 +0530

    Initial commit

---
+++ client/src/Pages/Contact.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../components/Header";
+
+const Contact = () => {
+  return (
+    <div className="home">
+      <Header />
+      <h1>Contact</h1>
+    </div>
+  );
+};
+
+export default Contact;
